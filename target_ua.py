b3fe54c105b3befda3da8abfa0274e09ae9ad807
Tetiana Vinnik <tetiana.vinnik@ucu.edu.ua>
2021-10-31 15:36:19 +0200

    Create function grt_words

--- target_ua.py
+++ target_ua.py
@@ -13,4 +13,36 @@ def generate_grid() -> List[List[str]]:
         if letter not in playground:
             playground.append(letter)
     return playground
-print(generate_grid())
+
+
+
+def get_words(file1, letters):
+    with open(file1, 'r') as file:
+        word_lst = []
+        for line in file:
+            line = line + ' '
+            line = line.strip().split()
+            word = line[0]
+            word = word.encode('cp1251', 'ignore').decode( 'utf-8', 'ignore')
+            if word != '':
+                line[0] = word
+                line = ' '.join(line)
+                if word[0] in letters and len(word) <= 5 :
+                    # line = line[:line.index(' ')]
+                    if '/adj' in line:
+                        part = 'adjective'
+                        line = [word, part]
+                    elif 'n' in line or 'noun' in line and 'noninfl' not in line and 'intj' not in line:
+                        part = 'noun'
+                        line = [word, part]
+                    elif '/v' in line:
+                        part = 'verb'
+                        line = [word, part]
+                    elif 'adv' in line:
+                        part = 'adverb'
+                        line = [word, part]
+                    word_lst.append(line)
+        print(word_lst)
+
+
+get_words("base.txt", generate_grid())
